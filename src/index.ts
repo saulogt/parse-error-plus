e1b1ddd364416122474b0e0f69ceaa2aa7d673ea
Saulo Tauil <saulogt@gmail.com>
2017-11-21 16:36:20 -0800

    First commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+
+export function setupParseError(parse) {
+  const oldError = parse.Error
+  function ParseError(code: number, message: string){
+    (Error as any).captureStackTrace(this)
+    oldError.call(this, code, message)
+    this.code = code;
+  }
+
+  oldError.prototype.__proto__ = Error.prototype
+  ParseError.prototype = oldError.prototype
+
+  parse.Error = ParseError
+  
+}
+
+declare global {
+  export namespace Parse {
+    interface Error {
+      stack: string;
+    }
+  }
+}
+
+
+
