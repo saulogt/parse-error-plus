7ea33c15866269cbb6b9b5f27f64285b3567379c
Saulo Tauil <saulogt@gmail.com>
2018-01-18 23:34:40 -0800

    Accepting an optional error decoder to be used in cloud code. As parse server creates a new error internally to send it to the client, it ends up loosing the stack information. This decoder extracts information from the error to recreate the stack

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 
-export function setupParseError(parse) {
+export type ParseErrorPlusDecoder = (code: number, message: string) => [number, string, string | undefined];
+
+export function setupParseError(parse, decoder?: ParseErrorPlusDecoder) {
   const oldError = parse.Error
 
   if (oldError.prototype.__proto__ === Error.prototype) {// Already converted
@@ -7,7 +9,15 @@ export function setupParseError(parse) {
   }
 
   function ParseError(code: number, message: string){
-    (Error as any).captureStackTrace(this)
+    let stack;
+    [code, message, stack] = decoder ? decoder(code, message) : [code, message, undefined];
+    
+    if (stack) {
+      this.stack = stack
+    } else {
+      (Error as any).captureStackTrace(this)
+    }
+    
     oldError.call(this, code, message)
     this.code = code;
   }
